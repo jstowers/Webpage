750ccfaa96d3d49d0460c0b0b23e93f0c93d4eea
Joe Stowers <greenchiletexan@gmail.com>
2017-02-24 00:04:42 -0700

    Refactor passionsdetail component to add musicVideos object and mapping function

--- src/components/passionsdetail.js
+++ src/components/passionsdetail.js
@@ -2,26 +2,41 @@ import React from 'react';
 
 const PassionsDetail = (props) => {
 
+	const musicVideos = [
+		{
+			title: 'Safari',
+			artist: 'J Balvin',
+			url: 'https://www.youtube.com/watch?v=JWESLtAKKlU',
+			img: '/img/PassionsSafari.png'
+		},
+		{
+			title: '35 Pa Las 12',
+			artist: 'Fuego',
+			url: 'https://www.youtube.com/watch?v=rh93WCJG1sM',
+			img: '/img/Passions35.png'
+		},
+		{
+			title: 'Lost On You',
+			artist: 'LP',
+			url: 'https://www.youtube.com/watch?v=wDjeBNv6ip0',
+			img: '/img/PassionsLost.png'
+		}
+	];
+
+	const videoItems = musicVideos.map(video => {
+		return (
+			<a href= {video.url} target="_blank">
+				<img 
+					className = 'video-link'
+					src= {video.img}
+					alt= {video.title} />
+			</a>
+		);
+	});
+
 	return (
 		<div>
-			<a 	href="https://www.youtube.com/watch?v=JWESLtAKKlU"
-				target="_blank">
-				<img 
-					className="video-link"
-					src= "/img/PassionsSafari.png" alt="Safari"/>
-			</a>
-			<a 	href="https://www.youtube.com/watch?v=rh93WCJG1sM"
-				target="_blank">
-				<img 
-					className="video-link"
-					src= "/img/Passions35.png" alt= "35PaLa12" />
-			</a>
-			<a 	href="https://www.youtube.com/watch?v=wDjeBNv6ip0"
-				target="_blank">
-				<img 
-					className="video-link"
-					src= "/img/PassionsLost.png" alt= "LostOnYou" />
-			</a>
+			{ videoItems }
 		</div>
 	);
 }
